468fc1e432f9a3f52ca3c2ea0f9eb249af079655
Yusuf Yilmaz <ysfyilmaz6@gmail.com>
2023-06-08 18:29:34 +0300

    Camera Updated
    Cameras rotation problem solved

--- GameJamInside/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ GameJamInside/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -163,7 +163,7 @@ namespace StarterAssets
 
         private void LateUpdate()
         {
-            CameraRotation();
+          
         }
 
         private void AssignAnimationIDs()
@@ -190,26 +190,7 @@ namespace StarterAssets
             }
         }
 
-        private void CameraRotation()
-        {
-            // if there is an input and camera position is not fixed
-            if (_input.look.sqrMagnitude >= _threshold && !LockCameraPosition)
-            {
-                //Don't multiply mouse input by Time.deltaTime;
-                float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
-
-                _cinemachineTargetYaw += _input.look.x * deltaTimeMultiplier;
-                _cinemachineTargetPitch += _input.look.y * deltaTimeMultiplier;
-            }
-
-            // clamp our rotations so our values are limited 360 degrees
-            _cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
-            _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
-
-            // Cinemachine will follow this target
-            CinemachineCameraTarget.transform.rotation = Quaternion.Euler(0 + 0,
-                _cinemachineTargetYaw, 0.0f);
-        }
+        
 
         private void Move()
         {
